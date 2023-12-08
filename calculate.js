6a903437e2408821d992a9e30f85f030a8ac2653
Wiebren <114926599+Wiebren-3@users.noreply.github.com>
2023-12-08 11:33:21 +0100

    give extra feedback on bmi

--- calculate.js
+++ calculate.js
@@ -5,25 +5,30 @@ function calculateBMI() {
     const weight = parseFloat(document.getElementById('weight').value);
   
     if (isNaN(age) || isNaN(height) || isNaN(weight)) {
-      alert('Vul alle velden in met geldige cijfers.');
-      return;
+        alert('Vul alle velden in met geldige cijfers.');
+        return;
     }
   
     const heightInMeters = height / 100;
     const bmi = weight / (heightInMeters * heightInMeters);
   
     let result = '';
+    let interpretation = '';
   
     if (bmi < 18.5) {
-      result = 'Veel te laag';
+        result = 'Veel te laag';
+        interpretation = 'Je BMI wijst op ondergewicht. Het is belangrijk om voldoende voedingsstoffen binnen te krijgen en mogelijk een arts of voedingsdeskundige te raadplegen.';
     } else if (bmi < 24.9) {
-      result = 'Goed';
+        result = 'Goed';
+        interpretation = 'Je BMI is in het gezonde bereik. Blijf actief en eet een evenwichtige voeding voor een gezonde levensstijl.';
     } else if (bmi < 29.9) {
-      result = 'Hoog';
+        result = 'Hoog';
+        interpretation = 'Je BMI wijst op overgewicht. Overweeg gezonde eetgewoonten en regelmatige lichaamsbeweging om je gezondheid te verbeteren.';
     } else {
-      result = 'Veel te hoog';
+        result = 'Veel te hoog';
+        interpretation = 'Je BMI geeft aan dat je obesitas hebt. Raadpleeg een gezondheidsprofessional voor advies over gewichtsbeheersing en een gezonde levensstijl.';
     }
   
-    document.getElementById('result').innerText = `Je BMI is ${bmi.toFixed(2)}. Resultaat: ${result}`;
-  }
-  
+    const bmiResult = `Je BMI is ${bmi.toFixed(2)}. Resultaat: ${result}. ${interpretation}`;
+    document.getElementById('result').innerText = bmiResult;
+}
